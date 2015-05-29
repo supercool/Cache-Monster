fb39a6552e7abdc247e79fb3c5b83b5e6a81f509
Josh Angell <josh@supercooldesign.co.uk>
2015-05-29 11:07:51 +0100

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Varnish
+
+A Craft CMS plugin to purge your Varnish cache. Requires the native `{% cache %}` tag to work.
---
+++ varnish/VarnishPlugin.php
@@ -0,0 +1,36 @@
+<?php
+namespace Craft;
+
+/**
+ * Varnish by Supercool
+ *
+ * @package   Varnish
+ * @author    Josh Angell
+ * @copyright Copyright (c) 2015, Supercool Ltd
+ * @link      http://plugins.supercooldesign.co.uk
+ */
+
+class VarnishPlugin extends BasePlugin
+{
+
+	public function getName()
+	{
+		return Craft::t('Varnish');
+	}
+
+	public function getVersion()
+	{
+		return '0.1';
+	}
+
+	public function getDeveloper()
+	{
+		return 'Supercool';
+	}
+
+	public function getDeveloperUrl()
+	{
+		return 'http://plugins.supercooldesign.co.uk';
+	}
+
+}

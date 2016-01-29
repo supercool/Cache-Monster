4d5e94d71eb9a88fd3312aecbf834cf7f79da2c5
Josh Angell <josh@supercooldesign.co.uk>
2016-01-29 09:50:51 +0000

    Ripped the logic from DeleteStaleTemplateCachesTask to work out which cache ids to delete - much more reliable.
    This fixes #2.

--- cachemonster/services/CacheMonsterService.php
+++ cachemonster/services/CacheMonsterService.php
@@ -12,55 +12,157 @@ namespace Craft;
 
 class CacheMonsterService extends BaseApplicationComponent
 {
+	// Properties
+	// =========================================================================
 
 	/**
-	 * [purgeElementById description]
+	 * @var
+	 */
+	private $_elementIds;
+
+	/**
+	 * @var
+	 */
+	private $_elementType;
+
+	/**
+	 * @var
+	 */
+	private $_batch;
+
+	/**
+	 * @var
+	 */
+	private $_batchRows;
+
+	/**
+	 * @var
+	 */
+	private $_noMoreRows;
+
+	/**
+	 * @var
+	 */
+	private $_cacheIdsToBeDeleted;
+
+	/**
+	 * @var
+	 */
+	private $_totalCriteriaRowsToBeDeleted;
+
+	/**
+	 * Gets the caches that are about to be deleted by the DeleteStaleTemplateCachesTask
+	 * and returns the paths for them.
+	 *
+	 * Basically nicked the logic from the DeleteStaleTemplateCachesTask
+	 * to work out which caches we are dealing with.
 	 *
 	 * @method getPaths
-	 * @param  int           $elementId the elementId of the element we want purging
-	 * @return array                    an array of paths to purge
+	 * @param  int           $element the element we want to purge
+	 * @return array                  an array of paths to purge
 	 */
-	public function getPaths($elementId)
+	public function getPaths($element)
 	{
+		$elementId = $element->id;
 
-		// Get the cache ids that relate to this element
-		$query = craft()->db->createCommand()
-			->selectDistinct('cacheId')
-			->from('templatecacheelements')
-			->where('elementId = :elementId', array(':elementId' => $elementId));
+		// What type of element(s) are we dealing with?
+		$this->_elementType = craft()->elements->getElementTypeById($elementId);
+		if (!$this->_elementType)
+		{
+			return 0;
+		}
 
-		$cacheIds = $query->queryColumn();
+		if (is_array($elementId))
+		{
+			$this->_elementIds = $elementId;
+		}
+		else
+		{
+			$this->_elementIds = array($elementId);
+		}
 
-		if ($cacheIds)
+		// Figure out how many rows we're dealing with
+		$totalRows = $this->_getQuery()->count('id');
+		$this->_batch = 0;
+		$this->_noMoreRows = false;
+		$this->_cacheIdsToBeDeleted = array();
+		$this->_totalCriteriaRowsToBeDeleted = 0;
+
+		// Loop each of the relavent rows in the `templatecachecriteria` table
+		for ($i=0; $i < $totalRows; $i++) {
+
+			// Do we need to grab a fresh batch?
+			if (empty($this->_batchRows))
+			{
+				if (!$this->_noMoreRows)
+				{
+					$this->_batch++;
+					$this->_batchRows = $this->_getQuery()
+						->order('id')
+						->offset(100*($this->_batch-1) - $this->_totalCriteriaRowsToBeDeleted)
+						->limit(100)
+						->queryAll();
+					// Still no more rows?
+					if (!$this->_batchRows)
+					{
+						$this->_noMoreRows = true;
+					}
+				}
+				if ($this->_noMoreRows)
+				{
+					return;
+				}
+			}
+
+			$row = array_shift($this->_batchRows);
+			// Have we already deleted this cache?
+			if (in_array($row['cacheId'], $this->_cacheIdsToBeDeleted))
+			{
+				$this->_totalCriteriaRowsToBeDeleted++;
+			}
+			else
+			{
+				// Create an ElementCriteriaModel that resembles the one that led to this query
+				$params = JsonHelper::decode($row['criteria']);
+				$criteria = craft()->elements->getCriteria($row['type'], $params);
+				// Chance overcorrecting a little for the sake of templates with pending elements,
+				// whose caches should be recreated (see http://craftcms.stackexchange.com/a/2611/9)
+				$criteria->status = null;
+				// See if any of the updated elements would get fetched by this query
+				if (array_intersect($criteria->ids(), $this->_elementIds))
+				{
+					// Delete this cache
+					$this->_cacheIdsToBeDeleted[] = $row['cacheId'];
+					$this->_totalCriteriaRowsToBeDeleted++;
+				}
+			}
+
+		}
+
+		if ($this->_cacheIdsToBeDeleted)
 		{
 
 			// Get the paths that those caches related to
 			$query = craft()->db->createCommand()
 				->selectDistinct('path')
 				->from('templatecaches')
-				->where(array('in', 'id', $cacheIds));
+				->where(array('in', 'id', $this->_cacheIdsToBeDeleted));
 
 			$paths = $query->queryColumn();
 
 			// Return an array of them
-			if ($paths)
-			{
+			if ($paths) {
 
-				if ( ! is_array($paths) )
-				{
+				if (!is_array($paths)) {
 					$paths = array($paths);
 				}
 
 				return $paths;
-			}
-			else
-			{
+			} else {
 				return false;
 			}
 
-		}
-		else
-		{
+		} else {
 			return false;
 		}
 
@@ -154,6 +256,28 @@ class CacheMonsterService extends BaseApplicationComponent
 			));
 		}
 
+	}
+
+	// Private Methods
+	// =========================================================================
+	/**
+	 * Returns a DbCommand object for selecting criteria that could be dropped by this task.
+	 *
+	 * @return DbCommand
+	 */
+	private function _getQuery()
+	{
+		$query = craft()->db->createCommand()
+			->from('templatecachecriteria');
+		if (is_array($this->_elementType))
+		{
+			$query->where(array('in', 'type', $this->_elementType));
+		}
+		else
+		{
+			$query->where('type = :type', array(':type' => $this->_elementType));
+		}
+		return $query;
 	}
 
 }

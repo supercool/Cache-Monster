5edba8f960ffbcba2a2d97a9a311019d94da4920
Josh Angell <josh@supercooldesign.co.uk>
2016-05-06 14:53:24 +0100

    The beginning of a new era, old with the old, in with the new.
    Well, copy and paste from the core to start with ...

--- README.md
+++ README.md
@@ -1,9 +1,20 @@
 # CacheMonster
 
-A Craft CMS plugin that keeps your cache permanently warm and optionally integrates with [Varnish](https://www.varnish-cache.org/).
+Caching on steroids for Craft CMS.
 
-The docs are [on our website](http://plugins.supercooldesign.co.uk/plugin/cache-monster) and you can read the story behind this plugin on our [engineering blog](http://supercool.github.io/2015/06/08/making-craft-sing-with-varnish-and-nginx.html).
 
-## Roadmap
-- Finish off documentation
-- Allow admins to set the paths of their site-maps in the cp
+## New roadmap
+
+- Fork the existing {% cache %} tag
+
+- Integrate with other cache drivers aside from the db (will still need to use the db to store the data as a first port of call, just then serve it from the other options if it exists) for example: flat file (whole pages only), memcache, redis etc.
+
+- Integrate with external caching solutions on top of the internal drivers such as Varnish, CloudFlare, AWS ElastiCache, Fastly etc.
+
+- Cache warming - keep all the drivers warm! When something has been purged for whatever reason, re-make it in the internal and (optionally) external caches.
+
+- Provide a CRON companion to the warming so that when other factors are in play other than a manual purge due to a save or delete operation things can still be kept warm - could operate as a separate service entirely as it could be a bit heavy for the main web server to be running. This tool could also purge on a schedule too perhaps.
+
+- When it purges or warms the cache, raise an event before and after so other interested parties can get involved.
+
+- Add flags / tags ala https://github.com/mmikkel/CacheFlag-Craft
--- cachemonster/CacheMonsterPlugin.php
+++ cachemonster/CacheMonsterPlugin.php
@@ -6,7 +6,7 @@ namespace Craft;
  *
  * @package   CacheMonster
  * @author    Josh Angell
- * @copyright Copyright (c) 2015, Supercool Ltd
+ * @copyright Copyright (c) 2016, Supercool Ltd
  * @link      http://plugins.supercooldesign.co.uk
  */
 
@@ -16,10 +16,6 @@ class CacheMonsterPlugin extends BasePlugin
 	// Properties
 	// =========================================================================
 
-	/**
-	 * @var
-	 */
-	private $_settings;
 
 	// Public Methods
 	// =========================================================================
@@ -31,7 +27,7 @@ class CacheMonsterPlugin extends BasePlugin
 
 	public function getVersion()
 	{
-		return '1.0.0';
+		return '2.0.0';
 	}
 
 	public function getDeveloper()
@@ -46,75 +42,6 @@ class CacheMonsterPlugin extends BasePlugin
 
 	public function init()
 	{
-
-		/**
-		 * Get plugin settings
-		 */
-		$plugin = craft()->plugins->getPlugin('cachemonster');
-		$this->_settings = $plugin->getSettings();
-
-		/**
-		 * Before we save, grab the paths that are going to be purged
-		 * and create a Task that will save them to a cache
-		 */
-		craft()->on('elements.onBeforeSaveElement', function(Event $event)
-		{
-
-			// Don’t bother doing anything if neither warming or purging is needed
-			if ($this->_settings['varnish'] || $this->_settings['warm'])
-			{
-
-				// Get the elementId
-				$elementId = $event->params['element']->id;
-
-				// NOTE: this works, but not if we run it in the Task!
-				//
-				// // What type of element(s) are we dealing with?
-				// $elementType = craft()->elements->getElementTypeById($elementId);
-				//
-				//
-				// $query = craft()->db->createCommand()
-				// 	->from('templatecachecriteria');
-				//
-				// if (is_array($elementType))
-				// {
-				// 	$query->where(array('in', 'type', $elementType));
-				// }
-				// else
-				// {
-				// 	$query->where('type = :type', array(':type' => $elementType));
-				// }
-				//
-				// // Figure out how many rows we're dealing with
-				// $totalRows = $query->count('id');
-				// Craft::dd($totalRows);
-
-				// Make a Task that will get the paths and cache them
-				$this->_makeTask('CacheMonster_CachePaths', $elementId);
-
-			}
-
-		});
-
-		/**
-		 *
-		 */
-		craft()->on('elements.onSaveElement', function(Event $event)
-		{
-
-			// Don’t bother doing anything if neither warming or purging is needed
-			if ($this->_settings['varnish'] || $this->_settings['warm'])
-			{
-
-				// Get the elementId
-				$elementId = $event->params['element']->id;
-
-				// Make the manager Task that will fire the SubTasks
-				craft()->tasks->createTask('CacheMonster_GetCachedPaths');
-
-			}
-
-		});
 
 	}
 
@@ -131,59 +58,11 @@ class CacheMonsterPlugin extends BasePlugin
 	protected function defineSettings()
 	{
 		return array(
-			'varnish' => array(AttributeType::Bool, 'default' => false),
-			'warm' => array(AttributeType::Bool, 'default' => true)
+			//
 		);
 	}
 
 	// Private Methods
 	// =========================================================================
-
-	/**
-	 * Registers a Task with Craft, taking into account if there
-	 * is already one pending
-	 *
-	 * @method makeTask
-	 * @param  string     $taskName   the name of the Task you want to register
-	 * @param  int|array  $elementId
-	 */
-	private function _makeTask($taskName, $elementId)
-	{
-
-		$task = craft()->tasks->getNextPendingTask($taskName);
-
-		if ($task && is_array($task->settings))
-		{
-			$settings = $task->settings;
-
-			if (!is_array($settings['elementId']))
-			{
-				$settings['elementId'] = array($settings['elementId']);
-			}
-
-			if (is_array($elementId))
-			{
-				$settings['elementId'] = array_merge($settings['elementId'], $elementId);
-			}
-			else
-			{
-				$settings['elementId'][] = $elementId;
-			}
-
-			// Make sure there aren't any duplicate element IDs
-			$settings['elementId'] = array_unique($settings['elementId']);
-
-			// Set the new settings and save the task
-			$task->settings = $settings;
-			craft()->tasks->saveTask($task, false);
-		}
-		else
-		{
-			craft()->tasks->createTask($taskName, null, array(
-				'elementId' => $elementId
-			));
-		}
-
-	}
 
 }

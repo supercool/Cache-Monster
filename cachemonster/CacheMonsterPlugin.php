eb08948ec58090ff496aa75490e22802e6b5207d
Josh Angell <josh@supercooldesign.co.uk>
2016-05-09 10:48:11 +0100

    Adding the db tables on install and dropping on uninstall.

--- cachemonster/CacheMonsterPlugin.php
+++ cachemonster/CacheMonsterPlugin.php
@@ -45,6 +45,28 @@ class CacheMonsterPlugin extends BasePlugin
 
 	}
 
+	public function onBeforeInstall()
+	{
+		$this->_createTemplateCacheTables();
+	}
+
+	public function onBeforeUninstall()
+	{
+		// drop our tables
+		if (craft()->db->tableExists('cachemonster_templatecachecriteria'))
+		{
+			craft()->db->createCommand()->dropTable('cachemonster_templatecachecriteria');
+		}
+		if (craft()->db->tableExists('cachemonster_templatecacheelements'))
+		{
+			craft()->db->createCommand()->dropTable('cachemonster_templatecacheelements');
+		}
+		if (craft()->db->tableExists('cachemonster_templatecaches'))
+		{
+			craft()->db->createCommand()->dropTable('cachemonster_templatecaches');
+		}
+	}
+
 	/**
 	 * @return CacheMonsterTwigExtension
 	 */
@@ -74,4 +96,51 @@ class CacheMonsterPlugin extends BasePlugin
 	// Private Methods
 	// =========================================================================
 
+	/**
+	 * Creates the template cache tables.
+	 *
+	 * Forked from the Craft core at 2.6.2784
+	 *
+	 * @return null
+	 */
+	private function _createTemplateCacheTables()
+	{
+		Craft::log('Creating the cachemonster_templatecaches table.');
+
+		craft()->db->createCommand()->createTable('cachemonster_templatecaches', array(
+			'cacheKey'   => array('column' => ColumnType::Varchar, 'null' => false),
+			'locale'     => array('column' => ColumnType::Locale, 'null' => false),
+			'path'       => array('column' => ColumnType::Varchar),
+			'expiryDate' => array('column' => ColumnType::DateTime, 'null' => false),
+			'body'       => array('column' => ColumnType::MediumText, 'null' => false),
+		), null, true, false);
+
+		craft()->db->createCommand()->createIndex('cachemonster_templatecaches', 'expiryDate,cacheKey,locale,path');
+		craft()->db->createCommand()->addForeignKey('cachemonster_templatecaches', 'locale', 'locales', 'locale', 'CASCADE', 'CASCADE');
+
+		Craft::log('Finished creating the cachemonster_templatecaches table.');
+		Craft::log('Creating the cachemonster_templatecacheelements table.');
+
+		craft()->db->createCommand()->createTable('cachemonster_templatecacheelements', array(
+			'cacheId'   => array('column' => ColumnType::Int, 'null' => false),
+			'elementId' => array('column' => ColumnType::Int, 'null' => false),
+		), null, false, false);
+
+		craft()->db->createCommand()->addForeignKey('cachemonster_templatecacheelements', 'cacheId', 'cachemonster_templatecaches', 'id', 'CASCADE', null);
+		craft()->db->createCommand()->addForeignKey('cachemonster_templatecacheelements', 'elementId', 'elements', 'id', 'CASCADE', null);
+
+		Craft::log('Finished creating the cachemonster_templatecacheelements table.');
+		Craft::log('Creating the cachemonster_templatecachecriteria table.');
+
+		craft()->db->createCommand()->createTable('cachemonster_templatecachecriteria', array(
+			'cacheId'  => array('column' => ColumnType::Int, 'null' => false),
+			'type'     => array('column' => ColumnType::Varchar, 'maxLength' => 150, 'null' => false),
+			'criteria' => array('column' => ColumnType::Text, 'null' => false),
+		), null, true, false);
+
+		craft()->db->createCommand()->addForeignKey('cachemonster_templatecachecriteria', 'cacheId', 'cachemonster_templatecaches', 'id', 'CASCADE', null);
+		craft()->db->createCommand()->createIndex('cachemonster_templatecachecriteria', 'type');
+
+		Craft::log('Finished creating the cachemonster_templatecachecriteria table.');
+	}
 }

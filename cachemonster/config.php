8b3cefbff49514fdea1b19552cc83fbd1f9a5327
Josh Angell <josh@supercooldesign.co.uk>
2016-05-20 13:07:52 +0100

    defaul the external service to `false`

--- cachemonster/config.php
+++ cachemonster/config.php
@@ -1,7 +1,7 @@
 <?php
 
 return array(
-	'externalCachingService' => 'Varnish', // Can be 'Varnish' or false
+	'externalCachingService' => false, // Can be 'Varnish' or false
 	'externalCachingServiceSettings' => array(
 		'url' => CRAFT_SITE_URL
 	)
